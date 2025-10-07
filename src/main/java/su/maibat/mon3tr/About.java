e5431fee1e1280df94e82201ecbef0135ee7e832
Al6ed0 <den06m@gmail.com>
2025-10-07 20:25:40 +0500

    Authors

--- src/main/java/su/maibat/mon3tr/About.java
+++ src/main/java/su/maibat/mon3tr/About.java
@@ -4,14 +4,15 @@ import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.telegram.telegrambots.meta.generics.TelegramClient;
 
-public class Authors extends Command
+public class About
 {
-    public final String name = "authors";
-    public final String helpInfo = "Something about command authors";
-    private String taleOfAuthors = "The great Mandrela, beloved member of human race, father to all sons";
+    public final String name = "about";
+    public final String helpInfo = "Something about command about";
+    private String informationAbout = "This bot will keep an eye on your deadlines and remind you of those that are coming," +
+            " but it doesn't know how to do that yet, but it will try its best.";
     void execute(Long chatId, TelegramClient telegramClient)
     {
-        SendMessage sendMessage = new SendMessage(chatId.toString(), taleOfAuthors);
+        SendMessage sendMessage = new SendMessage(chatId.toString(), informationAbout);
         try {
             telegramClient.execute(sendMessage);
         } catch (TelegramApiException e){
