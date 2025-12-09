586b0ad1c96bb1c3d9944ac3ff7d7ec44ae507e4
Al6ed0 <den06m@gmail.com>
2025-12-09 23:50:54 +0500

    Tech: UpdateOffsetCommandTest

--- src/test/java/su/maibat/mon3tr/commands/UpdateOffsetCommandTest.java
+++ src/test/java/su/maibat/mon3tr/commands/UpdateOffsetCommandTest.java
@@ -1,21 +1,13 @@
 package su.maibat.mon3tr.commands;
 
-import java.util.concurrent.BlockingQueue;
-import java.util.stream.Stream;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNull;
-
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
-
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 import org.mockito.ArgumentCaptor;
 import org.mockito.Mockito;
-
 import su.maibat.mon3tr.NumberedString;
 import su.maibat.mon3tr.commands.exceptions.CommandException;
 import su.maibat.mon3tr.db.DeadlineQuery;
@@ -23,17 +15,23 @@ import su.maibat.mon3tr.db.SQLiteLinker;
 import su.maibat.mon3tr.db.UserQuery;
 import su.maibat.mon3tr.db.exceptions.DeadlineNotFound;
 import su.maibat.mon3tr.db.exceptions.UserNotFound;
+import su.maibat.mon3tr.notifier.Notifier;
 
-public final class RemoveCommandTest {
+import java.util.concurrent.BlockingQueue;
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+
+public final class UpdateOffsetCommandTest {
     private final SQLiteLinker linker = Mockito.mock(SQLiteLinker.class);
-    private final DeadlineRemoveCommand remove = new DeadlineRemoveCommand(linker);
+    private final Notifier notifier = Mockito.mock(Notifier.class);
+    private final UpdateOffsetCommand updateOffset = new UpdateOffsetCommand(linker, notifier);
     private final BlockingQueue<NumberedString> responseQueue =
             (BlockingQueue<NumberedString>) Mockito.mock(BlockingQueue.class);
 
     @BeforeEach
     void setUp() throws UserNotFound, DeadlineNotFound {
-
-
         UserQuery user = new UserQuery(1, 1234, new int[]{});
         Mockito.when(linker.getUserById(1)).thenReturn(user);
 
@@ -55,16 +53,17 @@ public final class RemoveCommandTest {
         DeadlineQuery[] deadlinesForUser = {dl1, dl2, dl3, dl4};
 
         Mockito.when(linker.getDeadlinesForUser(1)).thenReturn(deadlinesForUser);
+        Mockito.when(linker.getDeadline(1)).thenReturn(dl1);
     }
 
     @ParameterizedTest(name = "Empty set")
     @MethodSource("emptyArgs")
-    @DisplayName("Add without arguments")
+    @DisplayName("Update without arguments")
     void withoutTest(final int inStateId, final int outStateId, final String answerText)
             throws CommandException, DeadlineNotFound {
 
-        State state = new State(inStateId, new String[]{}, remove);
-        State resultState = remove.execute(1, new String[]{}, state, responseQueue);
+        State state = new State(inStateId, new String[]{}, updateOffset);
+        State resultState = updateOffset.execute(1, new String[]{}, state, responseQueue);
 
         ArgumentCaptor<NumberedString> answerCaptor = ArgumentCaptor.forClass(NumberedString.class);
         if (inStateId == 0) {
@@ -84,7 +83,8 @@ public final class RemoveCommandTest {
     static Stream<Arguments> emptyArgs() {
         return Stream.of(
                 Arguments.of(0, 1, "Please enter a valid deadline id (number)"),
-                Arguments.of(1, 1, "Please enter a valid deadline id (number)")
+                Arguments.of(1, 1, "Please enter a valid deadline id (number)"),
+                Arguments.of(2, 2, "Please enter a offset (days before final date)")
         );
     }
 
@@ -94,11 +94,11 @@ public final class RemoveCommandTest {
     void withArgsTest(final int inStateId, final String answerText)
             throws CommandException, DeadlineNotFound {
 
-        State state = new State(inStateId, new String[]{}, remove);
-        if (inStateId == 1) {
+        State state = new State(inStateId, new String[]{}, updateOffset);
+        if (inStateId >= 1) {
             state.setMemory(new String[]{"1", "2", "3", "4"});
         }
-        State resultState = remove.execute(1, new String[]{"1"}, state, responseQueue);
+        State resultState = updateOffset.execute(1, new String[]{"1"}, state, responseQueue);
 
         ArgumentCaptor<NumberedString> answerCaptor = ArgumentCaptor.forClass(NumberedString.class);
         if (inStateId == 0) {
@@ -111,13 +111,18 @@ public final class RemoveCommandTest {
         assertEquals(1, answer.getNumber());
         assertEquals(answerText, answer.getString());
 
-        assertNull(resultState);
+        if (inStateId == 2) {
+            assertNull(resultState);
+        } else {
+            assertEquals(2, resultState.getStateId());
+        }
     }
 
     static Stream<Arguments> someArgs() {
         return Stream.of(
-                Arguments.of(0, "You have closed this gestalt!!!"),
-                Arguments.of(1, "You have closed this gestalt!!!")
+                Arguments.of(0, "Please enter a offset (days before final date)"),
+                Arguments.of(1, "Please enter a offset (days before final date)"),
+                Arguments.of(2, "Offset has been updated")
         );
     }
 
@@ -127,7 +132,7 @@ public final class RemoveCommandTest {
         DeadlineQuery[] queryList = new DeadlineQuery[]{};
         Mockito.when(linker.getDeadlinesForUser(1)).thenReturn(queryList);
 
-        State resultState = remove.execute(1, new String[]{"1"}, null,
+        State resultState = updateOffset.execute(1, new String[]{"1"}, null,
                 responseQueue);
         Mockito.verify(linker, Mockito.times(1)).getDeadlinesForUser(1);
 
@@ -141,30 +146,59 @@ public final class RemoveCommandTest {
         assertNull(resultState);
     }
 
-    @ParameterizedTest(name = "Illegal Args set")
-    @MethodSource("illegalArgs")
-    @DisplayName("Add with arguments")
-    void illegalArgsTest(final String arg, final boolean valid)
+    @ParameterizedTest(name = "Illegal Id Args set")
+    @MethodSource("illegalIdArgs")
+    @DisplayName("Add with Id arguments")
+    void illegalIdTest(final String arg, final boolean valid)
             throws CommandException {
 
-        State state = new State(1, new String[]{"1", "2", "3", "4"}, remove);
+        State state = new State(1, new String[]{"1", "2", "3", "4"}, updateOffset);
 
-        State resultState = remove.execute(1, new String[]{arg}, state, responseQueue);
+        State resultState = updateOffset.execute(1, new String[]{arg}, state, responseQueue);
 
         ArgumentCaptor<NumberedString> answerCaptor = ArgumentCaptor.forClass(NumberedString.class);
         Mockito.verify(responseQueue, Mockito.times(1)).add(answerCaptor.capture());
 
         NumberedString answer = answerCaptor.getValue();
         assertEquals(1, answer.getNumber());
-        assertEquals(valid, answer.getString().equals("You have closed this gestalt!!!"));
-        assertEquals(valid, resultState == null);
+        assertEquals(valid, answer.getString().equals("Please enter a offset "
+                + "(days before final date)"));
+        assertEquals(valid, resultState.getStateId() == 2);
     }
 
-    static Stream<Arguments> illegalArgs() {
+    static Stream<Arguments> illegalIdArgs() {
         return Stream.of(
                 Arguments.of("3", true),
                 Arguments.of("0", false),
                 Arguments.of("42", false)
+        );
+    }
+
+
+    @ParameterizedTest(name = "Illegal Offset Args set")
+    @MethodSource("illegalOffsetArgs")
+    @DisplayName("Add with Offset arguments")
+    void illegalOffsetTest(final String arg, final boolean valid)
+            throws CommandException {
+
+        State state = new State(2, new String[]{"1", "2", "3", "4"}, updateOffset);
+
+        State resultState = updateOffset.execute(1, new String[]{arg}, state, responseQueue);
+
+        ArgumentCaptor<NumberedString> answerCaptor = ArgumentCaptor.forClass(NumberedString.class);
+        Mockito.verify(responseQueue, Mockito.times(1)).add(answerCaptor.capture());
+
+        NumberedString answer = answerCaptor.getValue();
+        assertEquals(1, answer.getNumber());
+        assertEquals(valid, answer.getString().equals("Offset has been updated"));
+        assertEquals(valid, resultState == null);
+    }
+
+    static Stream<Arguments> illegalOffsetArgs() {
+        return Stream.of(
+                Arguments.of("3", true),
+                Arguments.of("-1", false),
+                Arguments.of("42", true)
         );
     }
 }
