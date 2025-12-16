efccdd5dcc707f25a9989b598682d4e61f817ce6
mandrela <none>
2025-12-16 21:37:05 +0500

    crypto sran'

--- src/main/java/su/maibat/mon3tr/db/GroupQuery.java
+++ src/main/java/su/maibat/mon3tr/db/GroupQuery.java
@@ -1,12 +1,16 @@
 package su.maibat.mon3tr.db;
 
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
 public final class GroupQuery extends DBQuery {
+    private static final String TOKEN_PREFIX = "t0k3n_";
+    private static final int TOKEN_LENGTH = 16;
+    private static final int MASK = 0xff;
     private static int salt = 0;
     private String name;
     private int ownerId;
     private String token;
-    private static final int MAGIC_NUMBER_4 = 4;
-    private static final int MAGIC_NUMBER_15 = 15;
 
 
     public GroupQuery() {
@@ -54,7 +58,27 @@ public final class GroupQuery extends DBQuery {
     }
 
     public String generateToken() {
-        return String.valueOf((name.hashCode() << MAGIC_NUMBER_15)
-                + (ownerId << MAGIC_NUMBER_4) + salt++);
+        try {
+            token = TOKEN_PREFIX + bytesToHex(
+                MessageDigest.getInstance("SHA-256").digest(
+                    (name + (salt++ << 1)).getBytes()
+                )
+            ).substring(0, TOKEN_LENGTH);
+            return token;
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException(e.getMessage());
+        }
+    }
+
+    private static String bytesToHex(final byte[] hash) {
+        StringBuilder hexString = new StringBuilder(2 * hash.length);
+        for (int i = 0; i < hash.length; i++) {
+            String hex = Integer.toHexString(MASK & hash[i]);
+            if (hex.length() == 1) {
+                hexString.append('0');
+            }
+            hexString.append(hex);
+        }
+        return hexString.toString();
     }
 }
